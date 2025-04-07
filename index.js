2ae3a6517e922478ecb83417be19ef1e7d658695
edfall1985 <edfall1985@gmail.com>
2025-04-07 14:43:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 const { execFile } = require("child_process");
-
 const ytdlp = new YTDlpWrap('./yt-dlp');
-
 const app = express();
 const port = process.env.PORT || 3000;
 
